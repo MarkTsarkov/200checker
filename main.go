f63f769afc9dc5c17efa6048e0aad9acbe9b5a4a
MarkTsarkov <51461489+MarkTsarkov@users.noreply.github.com>
2023-09-23 17:46:42 +0300

    solving..

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func MakeRequest() {
 	fmt.Println("Web-site to check status:\n")
 
 	reader := bufio.NewReader(os.Stdin) 
-	url, err := reader.ReadString(reader)
+	url, err := reader.ReadString()
 	if err != nil {
 		log.Fatalln(err)
 	}
